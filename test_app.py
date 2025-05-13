8b3260da644798f8e41e5ab74322e4ce701b467f
Héléna ROLDAN <helena.roldan@aidalinfo.fr>
2025-05-13 09:39:54 +0200

    Correction erreur test

--- test_app.py
+++ test_app.py
@@ -2,9 +2,9 @@ import pytest
 from app import app as flask_app # Renomme pour éviter conflit
 
 @pytest.fixture
-def app():2
+def app():
     """Crée une instance de l'application Flask pour les tests."""
-    yield flask_appi
+    yield flask_app
 
 @pytest.fixture
 def client(app):
